ad32cd7b11a2e529246d9994f81236e44e1f8bc0
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-21 19:15:23 -0300

    implements findById to Post Entity

---
+++ src/main/java/com/bruno/coursespringmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.bruno.coursespringmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruno.coursespringmongo.entities.Post;
+import com.bruno.coursespringmongo.repositories.PostRepositoy;
+import com.bruno.coursespringmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepositoy repo;
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+}
