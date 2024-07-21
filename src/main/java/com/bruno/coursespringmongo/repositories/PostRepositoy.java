18d0dbcbdbebb797a50cd031190cd90b927a2823
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-21 20:23:09 -0300

    add query using @Query annotation

--- src/main/java/com/bruno/coursespringmongo/repositories/PostRepositoy.java
+++ src/main/java/com/bruno/coursespringmongo/repositories/PostRepositoy.java
@@ -3,6 +3,7 @@ package com.bruno.coursespringmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bruno.coursespringmongo.entities.Post;
@@ -10,5 +11,8 @@ import com.bruno.coursespringmongo.entities.Post;
 @Repository
 public interface PostRepositoy extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searcTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);	
 }
