1a93da4dad664e4f9639f48d0114d37140625f39
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-21 16:36:25 -0300

    implements insert to User

--- src/main/java/com/bruno/coursespringmongo/services/UserService.java
+++ src/main/java/com/bruno/coursespringmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bruno.coursespringmongo.dto.UserDTO;
 import com.bruno.coursespringmongo.entities.User;
 import com.bruno.coursespringmongo.repositories.UserRepositoy;
 import com.bruno.coursespringmongo.services.exception.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
