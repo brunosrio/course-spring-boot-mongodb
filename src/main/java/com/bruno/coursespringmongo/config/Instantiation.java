ceb4fd8b4bb0b15ea557bf9fd767093e41e35525
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-21 19:02:37 -0300

    add Post ref to User

--- src/main/java/com/bruno/coursespringmongo/config/Instantiation.java
+++ src/main/java/com/bruno/coursespringmongo/config/Instantiation.java
@@ -40,9 +40,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paula, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		
 		postRepositoy.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepositoy.save(maria);
 	}
-
 }
