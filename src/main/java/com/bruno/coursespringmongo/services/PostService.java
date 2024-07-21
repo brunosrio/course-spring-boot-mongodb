e71f5af994b29acf2efda261a3861cc175411f82
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-21 20:50:50 -0300

    add query with multiple criteria

--- src/main/java/com/bruno/coursespringmongo/services/PostService.java
+++ src/main/java/com/bruno/coursespringmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.bruno.coursespringmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repo.searcTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 3600 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
