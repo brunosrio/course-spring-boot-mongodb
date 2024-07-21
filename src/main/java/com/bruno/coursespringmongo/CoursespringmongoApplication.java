39b72545ea7a8a3eea33b44943e1a98de18afd44
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-20 23:43:17 -0300

    initial files

---
+++ src/main/java/com/bruno/coursespringmongo/CoursespringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.coursespringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/coursespringmongo/CoursespringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.coursespringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
