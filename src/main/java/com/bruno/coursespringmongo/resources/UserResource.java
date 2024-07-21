c5c7733cf4de8c13579908dec8bbaba4be6e7b57
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-21 15:53:44 -0300

    implements UserDTO and refactor UserResource

--- src/main/java/com/bruno/coursespringmongo/resources/UserResource.java
+++ src/main/java/com/bruno/coursespringmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.bruno.coursespringmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bruno.coursespringmongo.dto.UserDTO;
 import com.bruno.coursespringmongo.entities.User;
 import com.bruno.coursespringmongo.services.UserService;
 
@@ -20,9 +22,10 @@ public class UserResource {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
