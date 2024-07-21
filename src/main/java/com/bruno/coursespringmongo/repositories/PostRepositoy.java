1486cdef9f8d7039d38c981bb190d4138c1f852f
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2024-07-21 20:15:21 -0300

    add query method to Post

--- src/main/java/com/bruno/coursespringmongo/repositories/PostRepositoy.java
+++ src/main/java/com/bruno/coursespringmongo/repositories/PostRepositoy.java
@@ -1,5 +1,7 @@
 package com.bruno.coursespringmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.bruno.coursespringmongo.entities.Post;
 
 @Repository
 public interface PostRepositoy extends MongoRepository<Post, String>{
-	
+
+	List<Post> findByTitleContainingIgnoreCase(String text);	
 }
